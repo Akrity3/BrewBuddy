b3c570e56e2c4987b1f0b842e9e9601dfae774a2
Akrity3 <163756385+Akrity3@users.noreply.github.com>
2025-07-12 22:44:41 +0545

    splash

--- app/src/main/java/com/example/brewbuddy/LoginActivity.kt
+++ app/src/main/java/com/example/brewbuddy/LoginActivity.kt
@@ -6,19 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.brewbuddy.ui.theme.BrewBuddyTheme
 
 class LoginActivity : ComponentActivity() {
@@ -31,8 +47,12 @@ class LoginActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun LoginBody() {
+    var username by remember { mutableStateOf("") } // for string datatype empty inverted comma
+    var password by remember { mutableStateOf("") }
+
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -53,6 +73,94 @@ fun LoginBody() {
                 contentScale = ContentScale.Crop
             )
 
+            // Semi-transparent overlay for readability
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha =0.4f))
+            )
+
+            // login form
+            // (column = vertical stacking , no overlay)
+
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Login",
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = {
+                        username = it
+                    },
+
+                    label = {
+                        Text(
+                            "Username",
+                            color = Color.White
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White.copy(alpha = 0.5f),
+                        cursorColor = Color.White
+                    ),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                )
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    label = {
+                        Text("Password",
+                            color = Color.White)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    visualTransformation = PasswordVisualTransformation(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White.copy(alpha = 0.5f),
+                        cursorColor = Color.White
+                    ),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+
+                )
+
+                Button(onClick = {
+//                   // TODO: handel login
+                },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Login")
+                }
+
+
+            }
 
 
 
--- app/src/main/java/com/example/brewbuddy/SplashScreen.kt
+++ app/src/main/java/com/example/brewbuddy/SplashScreen.kt
@@ -1,47 +1,128 @@
 package com.example.brewbuddy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.brewbuddy.ui.theme.BrewBuddyTheme
+import kotlinx.coroutines.delay
+import kotlin.jvm.java
 
 class SplashScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BrewBuddyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            SplashScreenBody()
+
         }
     }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SplashScreenBody() {
+
+    val context = LocalContext.current
+    val activity = context as ComponentActivity
+
+
+    val localemail = "" // to get the stored email SharedPreferences or DataStore
+
+    LaunchedEffect(Unit)
+    {
+        delay(3000)
+        if(localemail.toString().isEmpty()){
+            val intent = Intent(context, LoginActivity::class.java)
+            context.startActivity(intent)
+            activity.finish()
+        }
+        else{
+            val intent = Intent(context, DashboardActivity::class.java)
+            context.startActivity(intent)
+            activity.finish()
+        }
+    }
+
+
+
+
+
+
+
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     )
+    {
+        // background Image
+        Image(
+            painter = painterResource(R.drawable.splash_bg),
+            contentDescription = "Splash Background",
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
+
+        // Dark overlay
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f))
+
+        )
+
+        // Logo
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        )
+        {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(200.dp)
+            )
+
+        }
+    }
+
+
+
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
-    BrewBuddyTheme {
-        Greeting2("Android")
-    }
+fun SplashPreview() {
+    SplashScreenBody()
 }
